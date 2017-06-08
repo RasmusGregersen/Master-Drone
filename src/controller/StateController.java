2e54795912665db7755c87869fbcfe8ec067f9a9
Larsmyrup <larsvmj@gmail.com>
2017-06-08 15:03:47 +0200

    Defining states and TODO's for stateController

--- src/controller/StateController.java
+++ src/controller/StateController.java
@@ -2,6 +2,8 @@ package controller;
 
 import de.yadrone.base.IARDrone;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 /**
  * Created by Dave on 07/06/2017.
  */
@@ -10,7 +12,7 @@ public class StateController {
     private IARDrone drone;
 
     public enum Command {
-        TakeOff,Hover,QRSearch,QRFound,QRValidate,SearchForCircle,CircleFound,Centralize,FlyThrough,UpdateGate,Finish
+        ReadyForTakeOff,Hover,QRSearching,QRFound,QRValidated,QRCentralized,CircleFound,DroneCentralized,FlownThrough, Finished
     }
 
     public Command state;
@@ -18,76 +20,113 @@ public class StateController {
 
     private void commands(Command command){
         switch(command){
-            case TakeOff: takeOff();
+            case ReadyForTakeOff: takeOff();
                 break;
             case Hover: hover();
                 break;
-            case QRSearch: qRSearch();
+            case QRSearching: qRSearch();
                 break;
-            case QRFound: qRFound();
+            case QRFound: qRValidate();
                 break;
-            case QRValidate: qRValidate();
+            case QRValidated: qRCentralizing();
                 break;
-            case SearchForCircle: searchForCircle();
+            case QRCentralized: searchForCircle();
                 break;
-            case CircleFound: circleFound();
+            case CircleFound: centralize();
                 break;
-            case Centralize: centralize();
+            case DroneCentralized: flyThrough();
                 break;
-            case FlyThrough: flyThrough();
+            case FlownThrough: updateGate();
                 break;
-            case UpdateGate: updateGate();
-                break;
-            case Finish: finish();
+            case Finished: finish();
                 break;
         }
     }
 
 
     public void takeOff(){
-        System.out.println("TakeOff");
+        //Takeoff
+        System.out.println("ReadyForTakeOff");
         drone.takeOff();
+        //Check conditions and transit to next state
         state = Command.Hover;
+        commands(state);
     }
 
 
     public void hover() {
+        //Hover method
         System.out.println("Hover");
         drone.hover();
+        //Check conditions and transit to next state
+        state = Command.QRSearching;
+        commands(state);
     }
 
 
     public void qRSearch() {
+        //Searching method
         System.out.println("QRSearch");
+        //TODO: Implement qRSearch method
 
-    }
-
-    public void qRFound() {
-        System.out.println("QRSearch");
+        //Check conditions
+        //TODO: Implement check to see if QR tag is found. Keep looking or transit state
+        commands(state);
     }
 
     public void qRValidate() {
-        System.out.println("QRValidate");
+        //Validate QR method
+        System.out.println("QRValidation");
+        //TODO: Implement method to validate QR tag
+
+        //Check if validated and transit state
+        //TODO: Implement to check if validated and transit state. Otherwise move back to searching
+        commands(state);
+    }
+
+    public void qRCentralizing() {
+        //Centralize QR tag method
+        System.out.println("QRcentralizing");
+        //TODO: Implement method to centralize QR tag
+
+        //If succesfully centralized, transit otherwise move back to searching
+        //TODO: Implement check otherwise move back to search
     }
 
     public void searchForCircle() {
+        //Increase altitude and search for the circle
         System.out.println("SearchForCircle");
-    }
+        //TODO: Implement method to find circle
 
-    public void circleFound() {
-        System.out.println("CircleFound");
+        //Check if circle found and transit state
+        //TODO: Check if circle is found and transit state, otherwise back to search.
     }
 
     public void centralize() {
+        //Centralize drone in front of circle
         System.out.println("Centralize");
+        //TODO: Implement centralizing
+
+        //Check if drone is centralized and transit state or move back to searching.
+        //TODO: Check if drone i centralized, otherwise change state to circle search
     }
 
     public void flyThrough() {
+        //Flying through the ring
         System.out.println("FlyThrough");
+        //TODO: Implement flythrough
+
+        //Updating port to search for and transit state
+        //TODO: Transit state
     }
 
     public void updateGate() {
+        //Changing which QR tag to search for next
         System.out.println("UpdateGate");
+        //TODO: Implement port state changer
+
+        //Changing state to finishing or searching for next tag depending on port state
+        //TODO: Implement update gate and transit state
     }
 
     public void finish() {
